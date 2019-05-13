1520b5a9d3d38ca0877c2efdad70bdc6fa529a5f
Duc Vu <ducvu@localhost.localdomain>
2019-05-14 02:59:09 +0700

    latest commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const { enhancement: oldEnhancement } = item
+  
+  const enhancement = oldEnhancement == 20 ? oldEnhancement : oldEnhancement + 1
+  
+  return { 
+    ...item,
+    enhancement
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  const { enhancement: oldEnhancement, durability: oldDurability } = item
+  let newItem = {}
+  if (oldEnhancement < 15) {
+    newItem = { ...item, durability: oldDurability - 5 };
+  }
+
+  if (oldEnhancement >= 15) {
+    newItem = { ...item, durability: oldDurability - 10 };
+  }
+
+  if (oldEnhancement > 16) {
+    newItem = { ...newItem, enhancement: oldEnhancement - 1 };
+  }
+  return newItem
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+	describe('repair() method', () => {
+		it('returns a new item with durability of 100', () => {
+			const daHammah = {
+				name: 'da hammah', 
+				enhancement:15, 
+				durability: 50
+			}
+			const newHammah = enhancer.repair(daHammah)
+			expect(newHammah.durability).toBe(100) 
+		})
+	})
+
+	describe('succeed() method', () => {
+		const daHammah = {
+			name: 'da hammah', 
+			enhancement:15, 
+			durability: 50
+		}
+		const newHammah = enhancer.succeed(daHammah)
+
+		it('returns a new item with enhancement increased by 1', () => {
+			expect(newHammah.enhancement).toBe(15) 
+		})
+		it('returns a new item with the same durability', () => {
+			expect(newHammah.durability).toBe(50) 
+		})
+	})
+
+	describe('fail() method', () => {
+		const daHammah = {
+			name: 'da hammah', 
+			enhancement:15, 
+			durability: 50
+		}
+		const newHammah = enhancer.fail(daHammah)
+
+		it('returns a new item with enhancement increased by 1', () => {
+			expect(newHammah.enhancement).toBe(16) 
+		})
+		it('returns a new item with the same durability', () => {
+			expect(newHammah.durability).toBe(50) 
+		})
+	})
+})
+			
